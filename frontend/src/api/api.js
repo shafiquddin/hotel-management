a5bfb5215bdf7dbfe7fc449b3e1a356815dfa0df
shafiquddin <shafiquddin2k@gmail.com>
2026-08-18 21:58:46 +0530

    url changed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hotel-management-api-tpf8.onrender.com/api",
+  baseURL: "https://hotel-management-production-e60b.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
